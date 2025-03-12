f024450e9c8b77dd14c5ddb8d52bcde5412ed605
deepdados <63567106+deepdados@users.noreply.github.com>
2025-03-12 19:42:51 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Se quiser usar GitHub Pages, descomente e ajuste o basePath e assetPrefix:
+  // basePath: '/nome-do-repo',
+  // assetPrefix: '/nome-do-repo'
+};
+
+module.exports = nextConfig;
